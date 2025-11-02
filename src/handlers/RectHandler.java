665af5d9ca5a059027c93de81f684300589f6885
Soikat <193371447+soikat27@users.noreply.github.com>
2025-11-02 14:37:32 -0500

    Add JavaDoc to RectHandler class

--- src/handlers/RectHandler.java
+++ src/handlers/RectHandler.java
@@ -3,15 +3,45 @@ package handlers;
 import javafx.scene.input.MouseEvent;
 import shapes.Rect;
 
+/**
+ * Mouse event handler for drawing {@link Rect} shapes on a {@link ShapeCanvas}.
+ * <p>
+ * This class extends {@link DrawHandler} and overrides the {@code mousePressed} method
+ * to initialize a new {@link Rect} shape when the user presses the mouse.
+ * </p>
+ * 
+ * @author Soikat Saha
+ * @see DrawHandler
+ * @see ShapeCanvas
+ * @see Rect
+ * @see javafx.scene.input.MouseEvent
+ */
 public class RectHandler extends DrawHandler {
     
     // ----- CONSTRUCTORS -----
+
+    /**
+     * Constructs a {@code RectHandler} associated with the given {@link ShapeCanvas}.
+     *
+     * @param sc the {@link ShapeCanvas} where rectangles will be drawn
+     */
     public RectHandler (ShapeCanvas sc)
 	{
 		super(sc);
 	}
 
     // ----- METHODS -----
+
+    /**
+     * Called when the user presses the mouse to start drawing a rectangle.
+     * <p>
+     * This method creates a new {@link Rect} object and delegates to the
+     * parent {@link DrawHandler#mousePressed(MouseEvent)} to handle common behavior.
+     * </p>
+     *
+     * @param e the {@link MouseEvent} representing the mouse press
+     * @see DrawHandler#mousePressed(MouseEvent)
+     */
     @Override
 	public void mousePressed (MouseEvent e)
 	{
