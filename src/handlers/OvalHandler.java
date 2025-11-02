c7adad22a0354248a889dbc6afdc339951767e1a
Soikat <193371447+soikat27@users.noreply.github.com>
2025-11-02 14:43:29 -0500

    Add Javadoc to OvalHandler class

--- src/handlers/OvalHandler.java
+++ src/handlers/OvalHandler.java
@@ -3,15 +3,45 @@ package handlers;
 import javafx.scene.input.MouseEvent;
 import shapes.Oval;
 
+/**
+ * Mouse event handler for drawing {@link Oval} shapes on a {@link ShapeCanvas}.
+ * <p>
+ * This class extends {@link DrawHandler} and overrides the {@code mousePressed} method
+ * to initialize a new {@link Oval} shape when the user presses the mouse.
+ * </p>
+ * 
+ * @author Soikat Saha
+ * @see DrawHandler
+ * @see ShapeCanvas
+ * @see Oval
+ * @see javafx.scene.input.MouseEvent
+ */
 public class OvalHandler extends DrawHandler {
     
     // ----- CONSTRUCTORS -----
+
+    /**
+     * Constructs an {@code OvalHandler} associated with the given {@link ShapeCanvas}.
+     *
+     * @param sc the {@link ShapeCanvas} where ovals will be drawn
+     */
     public OvalHandler (ShapeCanvas sc)
 	{
 		super(sc);
 	}
 
     // ----- METHODS -----
+
+    /**
+     * Called when the user presses the mouse to start drawing an oval.
+     * <p>
+     * This method creates a new {@link Oval} object and delegates to the
+     * parent {@link DrawHandler#mousePressed(MouseEvent)} to handle common behavior.
+     * </p>
+     *
+     * @param e the {@link MouseEvent} representing the mouse press
+     * @see DrawHandler#mousePressed(MouseEvent)
+     */
     @Override
 	public void mousePressed (MouseEvent e)
 	{
