f9c42a8b468adab3bfd481aa6e194047414c7b15
Soikat <193371447+soikat27@users.noreply.github.com>
2025-11-02 13:37:32 -0500

    Add mousePressed(), mouseDragged(), mouseReleased() logic
    -- Implement mousePressed() logic to start drawing a new shape
    -- Implement mouseDragged() logic to update shape while dragging
    -- Implement mouseReleased() logic to finalize and add shape to canvas

--- src/handlers/DrawHandler.java
+++ src/handlers/DrawHandler.java
@@ -1,5 +1,6 @@
 package handlers;
 
+import javafx.geometry.Point2D;
 import shapes.MyShape;
 
 public class DrawHandler {
@@ -7,9 +8,47 @@ public class DrawHandler {
     protected MyShape     shape;
 	protected ShapeCanvas canvas;
 
+    // ----- CONSTRUCTORS -----
     public DrawHandler (ShapeCanvas sc)
 	{
 		canvas = sc;
 	}
 
+    // ----- METHODS -----
+    protected void mousePressed (MouseEvent e)
+	{
+		if (shape != null)
+		{	
+			double mx = e.getX();
+			double my = e.getY();
+
+			shape.setP1(new Point2D (mx, my));
+
+			canvas.setCurrShape(shape);
+		}
+	}
+
+    protected void mouseDragged (MouseEvent e)
+	{
+		if (shape != null)
+		{
+			double mx = e.getX();
+			double my = e.getY();
+
+			shape.setP2(mx, my);
+
+			canvas.paint();
+		}
+	}
+
+    protected void mouseReleased (MouseEvent e)
+	{
+		if (shape != null)
+		{
+			canvas.addShape(shape);
+			canvas.setCurrShape(null);
+
+			shape = null;
+		}
+	}
 }
