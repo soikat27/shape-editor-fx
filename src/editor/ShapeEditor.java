0c663d6ab32bce580fe003d6af08702b4214ec9e
Soikat <193371447+soikat27@users.noreply.github.com>
2025-11-05 23:47:41 -0500

    Setup controlPanel to the top of main BorderPane
    -- Add HBox control panel with Clear button and Filled checkbox
    -- Add Line, Oval, Rect radio buttons with ToggleGroup
    -- Add LineHandler, RectHandler, OvalHandler for shape drawing
    -- Implement Filled checkbox & Clear button

--- src/editor/ShapeEditor.java
+++ src/editor/ShapeEditor.java
@@ -4,10 +4,12 @@ import handlers.LineHandler;
 import handlers.OvalHandler;
 import handlers.RectHandler;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -45,7 +47,50 @@ public class ShapeEditor extends Application {
 
     public void setupControls ()
     {
+        controlPanel = new HBox (40);
+
+		// Add radio buttons
+		rbLine = new RadioButton ("Line");
+		rbOval = new RadioButton ("Oval");
+		rbRect = new RadioButton ("rectangle");
+
+		rbLine.setSelected(true);
+
+		ToggleGroup group = new ToggleGroup ();
+		rbLine.setToggleGroup(group);
+		rbOval.setToggleGroup(group);
+		rbRect.setToggleGroup(group);
+
+        lineHandler = new LineHandler (canvas);
+		rectHandler = new RectHandler (canvas);
+        ovalHandler = new OvalHandler (canvas);
+
+		rbLine.setOnAction(e -> {
+			canvas.replaceMouseHandler (lineHandler);
+		});
+		rbOval.setOnAction(e -> {
+			canvas.replaceMouseHandler (ovalHandler);
+		});
+		rbRect.setOnAction(e -> {
+			canvas.replaceMouseHandler (rectHandler);
+		});
+
+		// Add checkBox
+		cbFilled = new CheckBox ("Filled");
+		cbFilled.setOnAction(e -> {
+			canvas.setCurrFilled(cbFilled.isSelected());
+		});
+
+		// implement clear button
+		bnClear = new Button ("Clear");
+		bnClear.setOnAction(e -> {
+			canvas.clear();
+		});
+
+		controlPanel.getChildren().addAll(rbLine, rbRect, rbOval, cbFilled, bnClear);
+		controlPanel.setAlignment(Pos.BASELINE_LEFT);
         
+		mainPane.setTop(controlPanel);
     }
 
     public void setupCanvas ()
