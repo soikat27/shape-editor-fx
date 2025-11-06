6748ac0e01e4bed71db001344285a344b9710553
Soikat <193371447+soikat27@users.noreply.github.com>
2025-11-06 00:21:57 -0500

    Add file I/O methods for saving/loading shapes (text version)
    -- Implement toTextFile() method
    -- Implement fromTextFile() method

--- src/editor/ShapeCanvas.java
+++ src/editor/ShapeCanvas.java
@@ -1,13 +1,17 @@
 package editor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import shapes.MyShape;
+import shapes.*;
 
 public class ShapeCanvas extends Canvas {
     
@@ -106,5 +110,76 @@ public class ShapeCanvas extends Canvas {
 		setOnMousePressed(listener);
 		setOnMouseDragged(listener);
 		setOnMouseReleased(listener);
+	}
+
+	public void toTextFile (File fileObj)
+	{
+		try
+		{	
+			PrintWriter writer = new PrintWriter (fileObj);
+			
+			writer.println(shapes.size());
+			
+			for (MyShape shape : shapes)
+			{
+				writer.println(shape.toString());
+			}
+			
+			writer.close();
+		}
+		
+		catch (FileNotFoundException e)
+		{
+			System.out.println(fileObj + "couldn't be opened/created for writing");
+			e.printStackTrace();
+		}
+	}
+
+	public void fromTextFile (File fileObj)
+	{
+		try
+		{
+			Scanner reader = new Scanner (fileObj);
+			clear();
+			
+			int nShapes = reader.nextInt();
+			
+			for (int i = 0; i < nShapes; i++)
+			{
+				String type = reader.next();
+
+				double x1 = reader.nextDouble();
+				double y1 = reader.nextDouble();
+				double x2 = reader.nextDouble();
+				double y2 = reader.nextDouble();
+				
+				String isFilled = reader.next();
+				
+				double r = reader.nextDouble();
+				double g = reader.nextDouble();
+				double b = reader.nextDouble();
+				
+				MyShape shape;
+				
+				if (type.equalsIgnoreCase("Line")) 			shape = new Line (x1, y1, x2, y2);
+				else if (type.equalsIgnoreCase("Rectangle")) shape = new Rect (x1, y1, x2, y2);
+				else 													    shape = new Oval (x1, y1, x2, y2);
+				
+				if (isFilled.equalsIgnoreCase("true")) shape.setFilled(true);
+				
+				shape.setColor(Color.color(r, g, b));
+
+				shapes.add(shape);
+			}
+			
+			reader.close();
+			paint();
+		}
+		
+		catch (FileNotFoundException e)
+		{
+			System.out.println (fileObj + "couldn't be loaded for reading");
+			e.printStackTrace();
+		}
 	}
 }
