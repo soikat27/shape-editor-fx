495159a70ca67ba27d9d455eb689ea5bb9e18d13
Soikat <193371447+soikat27@users.noreply.github.com>
2025-11-16 15:40:16 -0500

    Add Javadoc comments in DeleteHandler class

--- src/edits/DeleteHandler.java
+++ src/edits/DeleteHandler.java
@@ -5,17 +5,36 @@ import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import shapes.MyShape;
 
+/**
+ * EventHandler implementation for handling mouse events to delete shapes from a canvas.
+ * 
+ * When the user clicks on the canvas, the shape closest to the click location
+ * is removed from the canvas.
+ */
 public class DeleteHandler implements EventHandler<MouseEvent> {
     
+    /** The ShapeCanvas on which shapes are drawn and deleted */
     private ShapeCanvas canvas;
 
     // ----- CONSTRUCTORS -----
+
+    /**
+     * Constructs a DeleteHandler with the specified ShapeCanvas.
+     *
+     * @param sc The ShapeCanvas to associate with this handler
+     */
     public DeleteHandler (ShapeCanvas sc)
 	{
 		canvas = sc;
 	}
 
     // ----- LOGICAL METHODS -----
+
+    /**
+     * Handles mouse click events to delete the shape closest to the click location.
+     *
+     * @param e The MouseEvent representing the mouse click
+     */
     @Override
 	public void handle (MouseEvent e)
 	{
