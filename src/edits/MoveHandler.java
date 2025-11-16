86f8ea6762a212439ad5bb3f725647d71aa8a6a3
Soikat <193371447+soikat27@users.noreply.github.com>
2025-11-16 15:53:04 -0500

    Implement MoveHandler class to move shapes
    -- Add move functionality that handles MOUSE_PRESSED to select closest shape, handles MOUSE_DRAGGED to move the selected shape
    and updates canvas on each move

---
+++ src/edits/MoveHandler.java
@@ -0,0 +1,72 @@
+package edits;
+
+import editor.ShapeCanvas;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import shapes.MyShape;
+
+public class MoveHandler implements EventHandler<MouseEvent> {
+    
+    private ShapeCanvas canvas;
+	private MyShape     closestShape;
+	private double x0, y0;
+	private double x1, y1;
+
+    // ----- CONSTRUCTORS -----
+    public MoveHandler (ShapeCanvas sc)
+	{
+		canvas = sc;
+	}
+
+    // ----- LOGICAL METHODS -----
+    private void mousePressed (MouseEvent e)
+	{
+		double mx = e.getX();
+		double my = e.getY();
+			
+		closestShape = canvas.closestShape(mx, my);
+
+		if (closestShape != null)
+		{
+			x0 = mx;
+			y0 = my;
+		}
+	}
+
+    private void mouseDragged (MouseEvent e)
+	{
+		x1 = e.getX();
+		y1 = e.getY();
+			
+		if (closestShape != null)
+		{
+			double dx = x1 - x0;
+			double dy = y1 - y0;
+				
+			closestShape.move(dx, dy);
+			canvas.paint();
+		}
+			
+		x0 = x1;
+		y0 = y1;
+	}
+
+    @Override
+	public void handle (MouseEvent e)
+	{
+        String eventName = e.getEventType().getName();
+		
+		switch (eventName)
+		{
+		case "MOUSE_PRESSED":
+			mousePressed(e);
+			break;
+		case "MOUSE_DRAGGED":
+			mouseDragged(e);
+			break;
+		default:
+			break;
+		}
+	}
+
+}
