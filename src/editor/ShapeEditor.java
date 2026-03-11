0f0ced789056dbc17af368e8b9a535d0ad2bf52b
Soikat <193371447+soikat27@users.noreply.github.com>
2026-03-11 01:49:41 -0400

    Modify ShapeEditor class, allowing access to newly implemented features
    -- Features include: move, delete, copy, group etc.

--- src/editor/ShapeEditor.java
+++ src/editor/ShapeEditor.java
@@ -2,9 +2,8 @@ package editor;
 
 import java.io.File;
 
-import handlers.LineHandler;
-import handlers.OvalHandler;
-import handlers.RectHandler;
+import edits.*;
+import handlers.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -35,10 +34,14 @@ public class ShapeEditor extends Application {
 	private HBox        controlPanel;
 	private Button      bnClear;
 	private CheckBox    cbFilled;
-	private RadioButton rbLine, rbOval, rbRect;
+	private RadioButton rbLine, rbOval, rbRect, rbDelete, rbMove, rbCopy, rbGroup;
 	private LineHandler lineHandler;
 	private RectHandler rectHandler;
 	private OvalHandler ovalHandler;
+	private DeleteHandler deleteHandler;
+	private MoveHandler   moveHandler;
+	private CopyHandler   copyHandler;
+	private GroupHandler  groupHandler;
 
 	private ColorPicker colorPicker;
 	private MenuBar     menuBar;
@@ -57,7 +60,7 @@ public class ShapeEditor extends Application {
 		Scene scene = new Scene (mainPane, APP_WIDTH, APP_HEIGHT);
 
 		mainStage.setScene(scene);
-		mainStage.setTitle("Shape Editor");
+		mainStage.setTitle("Shape Editor Application");
 		mainStage.show();
 	}
 
@@ -65,21 +68,34 @@ public class ShapeEditor extends Application {
     {
         controlPanel = new HBox (40);
 
+		lineHandler   = new LineHandler (canvas);
+		rectHandler   = new RectHandler (canvas);
+        ovalHandler   = new OvalHandler (canvas);
+		deleteHandler = new DeleteHandler (canvas);
+		moveHandler   = new MoveHandler (canvas);
+		copyHandler   = new CopyHandler (canvas);
+		groupHandler  = new GroupHandler (canvas);
+
 		// Add radio buttons
-		rbLine = new RadioButton ("Line");
-		rbOval = new RadioButton ("Oval");
-		rbRect = new RadioButton ("rectangle");
+		rbLine   = new RadioButton ("Line");
+		rbOval   = new RadioButton ("Oval");
+		rbRect   = new RadioButton ("rectangle");
+		rbDelete = new RadioButton ("Delete");
+		rbMove   = new RadioButton ("Move");
+		rbCopy   = new RadioButton ("Copy");
+		rbGroup  = new RadioButton ("Group");
 
 		rbLine.setSelected(true);
+		canvas.replaceMouseHandler (lineHandler);
 
 		ToggleGroup group = new ToggleGroup ();
 		rbLine.setToggleGroup(group);
 		rbOval.setToggleGroup(group);
 		rbRect.setToggleGroup(group);
-
-        lineHandler = new LineHandler (canvas);
-		rectHandler = new RectHandler (canvas);
-        ovalHandler = new OvalHandler (canvas);
+		rbDelete.setToggleGroup(group);
+		rbMove.setToggleGroup (group);
+		rbCopy.setToggleGroup(group);
+		rbGroup.setToggleGroup(group);
 
 		rbLine.setOnAction(e -> {
 			canvas.replaceMouseHandler (lineHandler);
@@ -89,6 +105,21 @@ public class ShapeEditor extends Application {
 		});
 		rbRect.setOnAction(e -> {
 			canvas.replaceMouseHandler (rectHandler);
+		});
+		rbDelete.setOnAction(e -> {
+			canvas.replaceMouseHandler (deleteHandler);
+		});
+		
+		rbMove.setOnAction(e -> {
+			canvas.replaceMouseHandler(moveHandler);
+		});
+		
+		rbCopy.setOnAction(e -> {
+			canvas.replaceMouseHandler(copyHandler);
+		});
+		
+		rbGroup.setOnAction(e -> {
+			canvas.replaceMouseHandler(groupHandler);
 		});
 
 		// Add checkBox
@@ -114,7 +145,7 @@ public class ShapeEditor extends Application {
 			canvas.clear();
 		});
 
-		controlPanel.getChildren().addAll(rbLine, rbRect, rbOval, cbFilled, bnClear);
+		controlPanel.getChildren().addAll(rbLine, rbRect, rbOval, cbFilled, colorPicker, rbDelete, rbMove, rbCopy, rbGroup, bnClear);
 		controlPanel.setAlignment(Pos.BASELINE_LEFT);
         
 		mainPane.setTop(controlPanel);
